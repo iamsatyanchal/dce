a4763e093a9851428a2f2e243058f354c600440f
Samsepi01 <131008348+harshbuttru3@users.noreply.github.com>
2026-03-13 16:19:20 +0530

    fixing cors error

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(cors({
   origin: function (origin, callback) {
     // allow requests with no origin (like mobile apps or curl requests)
     if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
+    if (allowedOrigins.indexOf(origin) === -1 && !origin.endsWith('.vercel.app')) {
       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
       return callback(new Error(msg), false);
     }
